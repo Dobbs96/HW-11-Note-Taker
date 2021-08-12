461e520bfb0fac4d9d5b279c55857f1cb7804eb3
James Dobbs <dobbs282@gmail.com>
2021-08-12 18:11:38 -0400

    post and delete working

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -16,8 +17,34 @@ app.get("/notes", (req, res) =>
   res.sendFile(path.join(`${__dirname}/public`, "notes.html"))
 );
 
-app.get("/api/notes", (req, res) => res.json());
+fs.readFile("db/db.json", (err, data) => {
+  if (err) throw err;
+  const notes = JSON.parse(data);
+  app.get("/api/notes", (req, res) => res.json(notes));
 
+  // POST
+
+  app.post("/api/notes", (req, res) => {
+    const newNote = req.body;
+    notes.push(newNote);
+    res.redirect("/");
+    update();
+  });
+
+  // DELETE
+  app.delete("/api/notes/:id", (req, res) => {
+    //   deletes first not target
+    notes.splice(req.params.id, 1);
+    res.redirect("/");
+    update();
+  });
+  const update = () => {
+    fs.writeFile("db/db.json", JSON.stringify(notes, "\t"), (err) => {
+      if (err) throw err;
+      return true;
+    });
+  };
+});
 app.listen(PORT, () =>
   console.log(`App listening on PORT http://localhost:${PORT}`)
 );
