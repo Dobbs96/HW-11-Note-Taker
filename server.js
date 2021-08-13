899856a3609fc796a482ec5f55f975dba401abaa
James Dobbs <dobbs282@gmail.com>
2021-08-13 17:07:33 -0400

    fixed errors and added new read files and update

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const { notDeepEqual } = require("assert");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -17,36 +18,71 @@ app.get("/notes", (req, res) =>
   res.sendFile(path.join(`${__dirname}/public`, "notes.html"))
 );
 
-fs.readFile("db/db.json", (err, data) => {
-  if (err) throw err;
-  const notes = JSON.parse(data);
-  app.get("/api/notes", (req, res) => res.json(notes));
-
-  // POST
-
-  app.post("/api/notes", (req, res) => {
-    const newNote = req.body;
-    notes.push(newNote);
-    res.redirect("/");
-    update();
-  });
-
-  // PUT
-  app.put("/api/notes/:id", (req, res) => {});
-  // DELETE
-  app.delete("/api/notes/:id", (req, res) => {
-    //   deletes first not target
-    notes.splice(req.params.id, 1);
-    res.redirect("/");
-    update();
-  });
-  const update = () => {
-    fs.writeFile("db/db.json", JSON.stringify(notes), (err) => {
-      if (err) throw err;
-      return true;
-    });
-  };
+// fs.readFile("db/db.json", (err, data) => {
+//   if (err) throw err;
+//   const notes = JSON.parse(data);
+// });
+let notes;
+app.get("/api/notes", (req, res) => {
+  read();
+  res.json(notes);
 });
+
+app.get("/api/notes/:id", (req, res) => {
+  const getID = req.params.id;
+});
+
+// let newID;
+// for (let i = 0; i < notes.length; i++) {
+//   newID = { id: i };
+// }
+// POST
+let addedID;
+let newNote;
+app.post("/api/notes", (req, res) => {
+  newNote = req.body;
+  read();
+  notes.push(newNote);
+
+  res.redirect("/");
+  update();
+});
+
+// PUT
+app.put("/api/notes/:id", (req, res) => {});
+
+// DELETE
+app.delete("/api/notes/:id", (req, res) => {
+  //   deletes first not target
+  reRead();
+  notes.splice(req.params.id, 1);
+  res.redirect("/");
+  update();
+  // console.log(notes);
+});
+
+const reRead = () => {
+  const data = fs.readFileSync("./db/db.json", "utf8");
+  notes = JSON.parse(data);
+};
+
+const read = () => {
+  const data = fs.readFileSync("./db/db.json", "utf8");
+  notes = JSON.parse(data);
+  for (let i = 0; i < notes.length; i++) {
+    addedID = i;
+    notes[i].id = addedID;
+  }
+  update();
+};
+
+const update = () => {
+  fs.writeFile("db/db.json", JSON.stringify(notes), (err) => {
+    if (err) throw err;
+    return true;
+  });
+};
+
 app.listen(PORT, () =>
   console.log(`App listening on PORT http://localhost:${PORT}`)
 );
